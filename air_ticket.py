c5fa01ea51583f7031087f2a1028f3e1540250ab
inkyum0714 <Inkyum0714@gmail.com>
2025-10-05 09:04:55 +0900

    1005

--- air_ticket.py
+++ air_ticket.py
@@ -1,14 +1,34 @@
 import requests
+import json
 
-url = "https://booking-com15.p.rapidapi.com/api/v1/flights/getMinPriceMultiStops"
+url = "https://booking-com15.p.rapidapi.com/api/v1/flights/searchFlights"
 
-querystring = {"legs":"[{'fromId':'BOM.AIRPORT','toId':'AMD.AIRPORT','date':'2024-05-25'},{'fromId':'AMD.AIRPORT','toId':'BOM.AIRPORT','date':'2024-05-26'}]","cabinClass":"FIRST","currency_code":"AED"}
+querystring = {
+    "fromId":"BOM.AIRPORT",
+    "toId":"DEL.AIRPORT",
+    "departDate":"2025-10-06",
+    "returnDate":"2025-10-07",
+    "stops":"none",
+    "pageNo":"1",
+    "adults":"1",
+    "children":"1",
+    "sort":"BEST",
+    "cabinClass":"ECONOMY",
+    "currency_code":"KRW"
+}
 
 headers = {
-	"x-rapidapi-key": "dd019e6396msh5b4c8d6763bb299p145742jsn20b7704bf7ae",
-	"x-rapidapi-host": "booking-com15.p.rapidapi.com"
+    "x-rapidapi-key": "dd019e6396msh5b4c8d6763bb299p145742jsn20b7704bf7ae",
+    "x-rapidapi-host": "booking-com15.p.rapidapi.com"
 }
 
 response = requests.get(url, headers=headers, params=querystring)
 
-print(response.json())
+# JSON으로 변환
+data = response.json()
+
+# 파일로 저장
+with open("flights.json", "w", encoding="utf-8") as f:
+    json.dump(data, f, ensure_ascii=False, indent=4)
+
+print("flights.json 파일로 저장 완료!")
