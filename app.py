4b2458af3958f5c97d9a841cb612b2dc5d1885bf
inkyum0714 <Inkyum0714@gmail.com>
2025-11-02 15:56:54 +0900

    진짜 찐 거의 완료

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ from service.weather import get_weather
 from service.service import find_country_by_city
 from service.air_ticket import air_ticket
 from service.risk import user_risk
+import traceback
 
 app = Flask(__name__)
 
@@ -43,7 +44,6 @@ def inputvalue():
                     user_input_traevel_data["city"].append(country_cities[0]["children"][i]["children"][j]["name"])
 
         country_cities_number = 0
-        print(user_input_traevel_data["city"])
         for key, value in krkr_airport.items():
             for city in user_input_traevel_data["city"]:
                 if value == city:
@@ -84,8 +84,6 @@ def weather():
     try:
         sum_score = 0
         translation_result = "translation_result.json"
-        with open(translation_result, "r", encoding="utf-8") as f:
-            krkr_airport = json.load(f)
         weather_result_list= []
         user_input_traevel_city = request.args.get("user_input_traevel_city")
         user_input_day = request.args.get("date")
@@ -95,12 +93,15 @@ def weather():
                 city,
                 get_weather(city.strip(), user_input_day)
             ])
-        print(weather_result_list[0][1][0])
         sum_score = sum(weather_result_list[0][1][0])
         total_weather_score = sum_score / len(weather_result_list[0][1][0])
         return jsonify({"message": "success","result_list": weather_result_list, "result": round(total_weather_score)})
     except Exception as e:
-        print(e)
+        print("예외 발생!")
+        print("예외 타입:", type(e))         # 예외 종류
+        print("예외 메시지:", e)            # 예외 메시지
+        print("트레이스백 상세:")
+        traceback.print_exc()  
         return jsonify({"message": "success","result": "외부 서버에서 응답하지 않습니다"})
 
 @app.route("/airticket", methods=["GET", "POST"])
@@ -142,14 +143,17 @@ def risk():
 
 @app.route("/total", methods=["GET", "POST"])
 def total():
-    print("총합 점수 계산 도착")
-    weather = float(request.args.get('weather', 0))
-    air_ticket = float(request.args.get('air_ticket', 0))
-    risk = float(request.args.get('risk', 0))
-    print(weather, air_ticket, risk)
-    total_data = round((30 + air_ticket - risk- weather)) 
-    print(total_data)
-    return jsonify({"message": "success","result": total_data})
+    try:
+        print("총합 점수 계산 도착")
+        weather = float(request.args.get('weather', 0))
+        air_ticket = float(request.args.get('air_ticket', 0))
+        risk = float(request.args.get('risk', 0))
+        print(weather, air_ticket, risk)
+        total_data = round((30 + air_ticket - risk- weather)) 
+        print(total_data)
+        return jsonify({"message": "success","result": total_data})
+    except ValueError:
+        return jsonify({"message": "error", "result": 0})
 
 
 
--- temp.py
+++ temp.py
@@ -1,14 +1,7 @@
-import requests
+import os
 
-url = "https://booking-com15.p.rapidapi.com/api/v1/flights/searchFlights"
+# Windows
+os.system('cls')
 
-querystring = {"fromId":"ICN.AIRPORT","toId":"FUK.AIRPORT","departDate":"2025-10-14","pageNo":"1","adults":"1","children":"1","sort":"CHEAPEST","cabinClass":"ECONOMY"}
-
-headers = {
-    	"x-rapidapi-host":"booking-com15.p.rapidapi.com",
-    	"x-rapidapi-key": "44b53a7a9fmshd4b09e186aeeefbp1eb70ejsnbca7d65497bd"
-    }
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())      
+# Mac/Linux
+# os.system('clear')
