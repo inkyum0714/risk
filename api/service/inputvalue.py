77c5739390e7781c5bc901ed8e50d64b0fc0c4b1
inkyum0714 <Inkyum0714@gmail.com>
2025-11-08 16:03:17 +0900

    그럼 제가 선배 맘에

--- api/service/inputvalue.py
+++ api/service/inputvalue.py
@@ -1,66 +1,73 @@
 import json, os
-def inputvalue_service(user_input_traevel):
+def inputvalue_service(user_input_trave):
     country_cities_number = 0
-    translation_result = "translation_result.json"
-    country_airport = os.path.join("data", "country_airport.json")
-    country_cities = os.path.join("data", "country_cities.json")
-    country_code_data_five = os.path.join("data", "country_code_data_five.json")
-    country_code_data_three = os.path.join("data", "country_code_data_three.json")
-    with open(translation_result, "r", encoding="utf-8") as f:
+    BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+    translation_result_path = os.path.join(BASE_DIR, "translation_result.json")
+    country_airport_path = os.path.join(BASE_DIR, "country_airport.json")
+    country_cities_path = os.path.join(BASE_DIR, "country_cities.json")
+    country_code_data_five_path = os.path.join(BASE_DIR, "country_code_data_five.json")
+    country_code_data_three_path = os.path.join(BASE_DIR, "country_code_data_three.json")
+
+    # JSON 읽기
+    with open(translation_result_path, "r", encoding="utf-8") as f:
         krkr_airport = json.load(f)
-    with open(country_airport, "r", encoding="utf-8") as f:
+
+    with open(country_airport_path, "r", encoding="utf-8") as f:
         country_airport = json.load(f)
-    with open(country_cities, "r", encoding="utf-8") as f:
+
+    with open(country_cities_path, "r", encoding="utf-8") as f:
         country_cities = json.load(f)
-    with open(country_code_data_five, "r", encoding="utf-8") as f:
+
+    with open(country_code_data_five_path, "r", encoding="utf-8") as f:
         country_code_data_five_number = json.load(f)
-    with open(country_code_data_three, "r", encoding="utf-8") as f:
-        country_code_data_three_number = json.load(f)     
-    user_input_traevel_data = {
+
+    with open(country_code_data_three_path, "r", encoding="utf-8") as f:
+        country_code_data_three_number = json.load(f)    
+    user_input_trave_data = {
         "country": "",
         "city": [],
         "airport": []
     }
-    print(user_input_traevel)
-    if user_input_traevel in country_code_data_three_number: #나라
-        user_input_traevel_type = "country"
-        print(user_input_traevel_type)
-        user_input_traevel_data["country"] = user_input_traevel
+    print(user_input_trave)
+    if user_input_trave in country_code_data_three_number: #나라
+        user_input_trave_type = "country"
+        print(user_input_trave_type)
+        user_input_trave_data["country"] = user_input_trave
         for i in range(len(country_cities[0]["children"])):
-            if country_cities[0]["children"][i]["name"] == user_input_traevel:
+            if country_cities[0]["children"][i]["name"] == user_input_trave:
                 print("옴")
                 for j in range(len(country_cities[0]["children"][i]["children"])):
-                    user_input_traevel_data["city"].append(country_cities[0]["children"][i]["children"][j]["name"])
+                    user_input_trave_data["city"].append(country_cities[0]["children"][i]["children"][j]["name"])
         country_cities_number = 0
         for key, value in krkr_airport.items():
-            for city in user_input_traevel_data["city"]:
+            for city in user_input_trave_data["city"]:
                 if value == city:
-                    user_input_traevel_data["airport"].append(key)
-        return user_input_traevel_data
+                    user_input_trave_data["airport"].append(key)
+        return user_input_trave_data
 
-    elif user_input_traevel in country_airport: #공항
-        user_input_traevel_type = "airport"
-        print(user_input_traevel_type)
+    elif user_input_trave in country_airport: #공항
+        user_input_trave_type = "airport"
+        print(user_input_trave_type)
         for i in range(len(country_cities[0]["children"])):
             for j in range(len(country_cities[0]["children"][i]["children"])):
-                if country_cities[0]["children"][i]["children"][j]["name"] == krkr_airport[user_input_traevel]:
-                    user_input_traevel_data["country"] = country_cities[0]["children"][i]["name"]
+                if country_cities[0]["children"][i]["children"][j]["name"] == krkr_airport[user_input_trave]:
+                    user_input_trave_data["country"] = country_cities[0]["children"][i]["name"]
                     break
-        user_input_traevel_data["city"] = [krkr_airport[user_input_traevel]]
-        user_input_traevel_data["airport"] = user_input_traevel
-        return user_input_traevel_data
+        user_input_trave_data["city"] = [krkr_airport[user_input_trave]]
+        user_input_trave_data["airport"] = user_input_trave
+        return user_input_trave_data
     
-    elif user_input_traevel in country_code_data_five_number: #도시
-        user_input_traevel_type = "city"
-        print(user_input_traevel_type)
+    elif user_input_trave in country_code_data_five_number: #도시
+        user_input_trave_type = "city"
+        print(user_input_trave_type)
         for i in range(len(country_cities[0]["children"])):
             for j in range(len(country_cities[0]["children"][i]["children"])):
-                if country_cities[0]["children"][i]["children"][j]["name"] == user_input_traevel:
-                    user_input_traevel_data["country"] = country_cities[0]["children"][i]["name"]
+                if country_cities[0]["children"][i]["children"][j]["name"] == user_input_trave:
+                    user_input_trave_data["country"] = country_cities[0]["children"][i]["name"]
                     break
         for key, value in krkr_airport.items():
-            if value == user_input_traevel:
-                user_input_traevel_data["airport"].append(key)
+            if value == user_input_trave:
+                user_input_trave_data["airport"].append(key)
                 country_cities_number += 1
-        user_input_traevel_data["city"] = [user_input_traevel]
-        return user_input_traevel_data
+        user_input_trave_data["city"] = [user_input_trave]
+        return user_input_trave_data
