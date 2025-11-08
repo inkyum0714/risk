73ed91343c4bf41f70ee23956d85ad8bca40705b
inkyum0714 <Inkyum0714@gmail.com>
2025-11-08 16:17:03 +0900

    한쪽 무릎으로 될줄 알았냐

--- api/service/inputvalue.py
+++ api/service/inputvalue.py
@@ -1,30 +1,30 @@
 import json, os
 def inputvalue_service(user_input_trave):
     country_cities_number = 0
-    current_dir = os.path.dirname(__file__) 
-    translation_result_path = os.path.join(current_dir, "..", "translation_result.json")  # 한 단계 위로
+    current_dir = os.path.dirname(__file__)  # inputvalue.py가 있는 폴더(api/service)
+    
+    # data 폴더에 있는 json 파일 경로 지정
+    country_airport_path = os.path.join(current_dir, "..", "data", "country_airport.json")
+    country_cities_path = os.path.join(current_dir, "..", "data", "country_cities.json")
+    country_code_data_five_path = os.path.join(current_dir, "..", "data", "country_code_data_five.json")
+    country_code_data_three_path = os.path.join(current_dir, "..", "data", "country_code_data_three.json")
+    translation_result_path = os.path.join(current_dir, "..", "translation_result.json")
+    
+    # JSON 파일 읽기
     with open(translation_result_path, "r", encoding="utf-8") as f:
         krkr_airport = json.load(f)
-    country_airport_path = os.path.join(current_dir, "country_airport.json")
-    country_cities_path = os.path.join(current_dir, "country_cities.json")
-    country_code_data_five_path = os.path.join(current_dir, "country_code_data_five.json")
-    country_code_data_three_path = os.path.join(current_dir, "country_code_data_three.json")
-
-    # JSON 읽기
-    with open(translation_result_path, "r", encoding="utf-8") as f:
-        krkr_airport = json.load(f)
-
+    
     with open(country_airport_path, "r", encoding="utf-8") as f:
         country_airport = json.load(f)
-
+    
     with open(country_cities_path, "r", encoding="utf-8") as f:
         country_cities = json.load(f)
-
+    
     with open(country_code_data_five_path, "r", encoding="utf-8") as f:
         country_code_data_five_number = json.load(f)
-
+    
     with open(country_code_data_three_path, "r", encoding="utf-8") as f:
-        country_code_data_three_number = json.load(f)    
+        country_code_data_three_number = json.load(f) 
     user_input_trave_data = {
         "country": "",
         "city": [],
